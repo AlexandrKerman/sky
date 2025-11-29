1f0c2b46a9bb71fe05fb82f6c796d4be31c579d4
Kerbin Original <aakolomeycev@gmail.com>
2025-11-29 21:25:17 +0300

    В модуле widget.py реализована функция get_date

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ def main() -> None:
     Ничего не принимает, ничего не возвращает
     """
 
-    print(widget.mask_account_card('Visa Platinum 7000792289606361'))
+    print(widget.mask_account_card("Visa Platinum 7000792289606361"))
+    print(widget.get_date("2024-03-11T02:26:18.671407"))
 
 
 if __name__ == "__main__":
--- src/widget.py
+++ src/widget.py
@@ -1,3 +1,5 @@
+import datetime
+
 from src import masks
 
 
@@ -6,14 +8,24 @@ def mask_account_card(user_card: str) -> str:
     Принимает тип и номер карты/счёта в виде str.
     Возвращает Тип карты и замаскированную карту/счёт.
     """
-    user_card_number = ''.join([i for i in user_card if i.isdigit()])
+    user_card_number = "".join([i for i in user_card if i.isdigit()])
     user_card_number_length = len(user_card_number)
     if user_card_number_length == 20:
         masked_user_card = masks.get_mask_account(user_card_number)
     elif user_card_number_length == 16:
         masked_user_card = masks.get_mask_card_number(user_card_number)
     else:
-        raise ValueError('Invalid card/account number. Expected sequence of 16 or 20 digits.')
-    user_card_type = user_card[:len(user_card) - user_card_number_length]
+        raise ValueError("Invalid card/account number. Expected sequence of 16 or 20 digits.")
+    user_card_type = user_card[: len(user_card) - user_card_number_length]
     masked_user_card = user_card_type + masked_user_card
     return masked_user_card
+
+
+def get_date(date: str) -> str:
+    """
+    Принимает на вход неотформатированную дату в формате
+    yyyy-dd-mmThh:mm:ss.ssssss
+    """
+    parsed_date = datetime.datetime.strptime(date, "%Y-%m-%dT%H:%M:%S.%f")
+    formatted_date = parsed_date.strftime("%d.%m.%Y")
+    return formatted_date
