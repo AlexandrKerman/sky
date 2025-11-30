5d6f71e30431dc9b4f8af4d08556e978a6917000
Kerbin Original <aakolomeycev@gmail.com>
2025-11-30 18:57:34 +0300

    Создан и заполнен README.md, Сформирован файл зависимостей requirements.txt, Исправлено форматирование кода

--- src/processing.py
+++ src/processing.py
@@ -1,12 +1,9 @@
-from distlib.database import new_dist_class
-
-
-def filter_by_state(dict_list: list[dict], state: str = 'EXECUTED') -> list[dict]:
+def filter_by_state(dict_list: list[dict], state: str = "EXECUTED") -> list[dict]:
     """
     Принимает на вход список словарей и опциональный ключ (По умолчанию 'EXECUTED').
     Возвращает список словарей, где значение state = опциональному ключу.
     """
-    new_dict_list = [i for i in dict_list if i.get('state') == state]
+    new_dict_list = [i for i in dict_list if i.get("state") == state]
     return new_dict_list
 
 
@@ -15,6 +12,5 @@ def sort_by_date(dict_list: list[dict], reverse: bool = True) -> list[dict]:
     Принимает на вход список словарей и опциональный ключ порядка сортировки.
     Возвращает новый список словарей, отсортированный по дате (по умолчанию по убыванию)
     """
-    sort_key = lambda x: x.get('date')
-    new_dict_list = sorted(dict_list, key=sort_key, reverse=reverse)
+    new_dict_list = sorted(dict_list, key=lambda x: x.get("date", ""), reverse=reverse)
     return new_dict_list
