7540e08a6ac6c0f10210610ec31726a4ab3a8860
Kerbin Original <aakolomeycev@gmail.com>
2025-11-30 18:22:16 +0300

    Реализована функция sort_by_date в модуле processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,7 +1,20 @@
+from distlib.database import new_dist_class
+
+
 def filter_by_state(dict_list: list[dict], state: str = 'EXECUTED') -> list[dict]:
     """
     Принимает на вход список словарей и опциональный ключ (По умолчанию 'EXECUTED').
     Возвращает список словарей, где значение state = опциональному ключу.
     """
     new_dict_list = [i for i in dict_list if i.get('state') == state]
+    return new_dict_list
+
+
+def sort_by_date(dict_list: list[dict], reverse: bool = True) -> list[dict]:
+    """
+    Принимает на вход список словарей и опциональный ключ порядка сортировки.
+    Возвращает новый список словарей, отсортированный по дате (по умолчанию по убыванию)
+    """
+    sort_key = lambda x: x.get('date')
+    new_dict_list = sorted(dict_list, key=sort_key, reverse=reverse)
     return new_dict_list
