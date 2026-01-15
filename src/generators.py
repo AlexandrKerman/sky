d34fde553276c94866ffd6305242af655c1086cb
Kerbin Original <aakolomeycev@gmail.com>
2026-01-15 18:05:59 +0300

    Добавлены тесты на генераторы

--- src/generators.py
+++ src/generators.py
@@ -7,31 +7,30 @@ def filter_by_currency(dict_list: list[dict], currency: str):
     if isinstance(dict_list, list):
         for i in dict_list:
             try:
-                if i['operationAmount']['currency']['code'] == currency:
+                if i["operationAmount"]["currency"]["code"] == currency:
                     yield i
             except KeyError:
                 continue
     else:
-        raise TypeError('Got not a list')
+        raise TypeError("Got not a list")
 
 
 def transaction_description(dict_list: list[dict]):
     if isinstance(dict_list, list):
         for i in dict_list:
             try:
-                yield i['description']
+                yield i["description"]
             except KeyError:
                 yield None
     else:
-        raise TypeError('Got not a list')
+        raise TypeError("Got not a list")
 
 
 def card_number_generator(start: int, end: int):
     if start < 1:
         start = 1
-    card_generator = ('0' * (16 - len(str(i))) + str(i) for i in range (start, end + 1))
+    card_generator = ("0" * (16 - len(str(i))) + str(i) for i in range(start, end + 1))
     for i in card_generator:
         if len(i) > 16:
             return
         yield f"{i[:4]} {i[4:8]} {i[8:12]} {i[12:]}"
-
