cf1000500e5c1042af237e6f4b90b828d10a7d0f
Kerbin Original <aakolomeycev@gmail.com>
2026-02-01 19:18:01 +0300

    Добавлены новые фичи в processing.py: поиск по слову и счётчик операций по категориям

--- src/processing.py
+++ src/processing.py
@@ -1,13 +1,12 @@
 import re
-
+from collections import Counter
 
 def filter_by_state(dict_list: list[dict], state: str = "EXECUTED") -> list[dict]:
     """
     Принимает на вход список словарей и опциональный ключ (По умолчанию 'EXECUTED').
     Возвращает список словарей, где значение state = опциональному ключу.
     """
-    new_dict_list = [i for i in dict_list if i.get("state") == state]
-    return new_dict_list
+    return [i for i in dict_list if i.get("state") == state]
 
 
 def sort_by_date(dict_list: list[dict], reverse: bool = True) -> list[dict]:
@@ -15,13 +14,20 @@ def sort_by_date(dict_list: list[dict], reverse: bool = True) -> list[dict]:
     Принимает на вход список словарей и опциональный ключ порядка сортировки.
     Возвращает новый список словарей, отсортированный по дате (по умолчанию по убыванию)
     """
-    new_dict_list = sorted(dict_list, key=lambda x: x.get("date", ""), reverse=reverse)
-    return new_dict_list
+    return sorted(dict_list, key=lambda x: x.get("date", ""), reverse=reverse)
 
 
-def process_bank_search(data: list[dict]) -> list[dict]:
-    pass
+def process_bank_search(data: list[dict], search: str) -> list[dict]:
+    """
+    Принимает list[dict] с ключами 'description' и слово для поиска search (str).
+    Возвращает list[dict], где было найдено search по ключу 'description'.
+    """
+    return [i for i in data if re.search(search, str(i.get('description')), flags=re.IGNORECASE)]
 
 
-def process_bank_operations(data: list[dict]) -> list[dict]:
-    pass
+def process_bank_operations(data: list[dict], categories: list) -> dict:
+    """
+    Принимает data: list[dict] и categories: list
+    Возвращает dict в виде {'category': count}
+    """
+    return Counter([description for i in data if (description := i.get('description')) in categories])
