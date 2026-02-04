b3b48b65d2e9790e5de724fb9b97924f0066ad20
Kerbin Original <aakolomeycev@gmail.com>
2026-02-04 11:32:19 +0300

    Дописаны тесты к новому функционалу

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -27,3 +27,16 @@ def test_sort_by_date(dict_list_raw_data):
 
 def test_sort_by_date_invalid():
     assert processing.sort_by_date([]) == []
+
+
+def test_bank_search():
+    data = [{'description': 'some_Description'}, {'description': 'some_another_description'}, {'description': 'no'}, {}]
+    result = processing.process_bank_search(data, search='description')
+    assert result == [{'description': 'some_Description'}, {'description': 'some_another_description'}]
+
+
+def rest_bank_operations():
+    data = [{'description': 'some_Description'}, {'description': 'some_another_description'}, {'description': 'no'}, {'description': 'no'}, {}]
+    result = processing.process_bank_operations(data, categories=['some_Description', 'no'])
+    assert result == {'some_Description': 1, 'no': 2}
+
